478fc05444796301c86d0bb3c692798ba3c3c377
Baptiste Langlade <baptouuuu@gmail.com>
2021-03-06 17:16:42 +0100

    add lazy connection

---
+++ src/Connection/Lazy.php
@@ -0,0 +1,32 @@
+<?php
+declare(strict_types = 1);
+
+namespace Formal\AccessLayer\Connection;
+
+use Formal\AccessLayer\{
+    Connection,
+    Query,
+};
+use Innmind\Url\Url;
+use Innmind\Immutable\Sequence;
+
+final class Lazy implements Connection
+{
+    private Url $url;
+    private ?Connection $connection = null;
+
+    public function __construct(Url $url)
+    {
+        $this->url = $url;
+    }
+
+    public function __invoke(Query $query): Sequence
+    {
+        return ($this->connection())($query);
+    }
+
+    private function connection(): Connection
+    {
+        return $this->connection ?? $this->connection = new PDO($this->url);
+    }
+}
