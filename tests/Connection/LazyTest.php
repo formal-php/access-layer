d131b52e4a4a2404139e980e38f7cffb2803ee42
Baptiste Langlade <baptouuuu@gmail.com>
2022-03-06 12:53:45 +0100

    allow to chose the connection to lazy load

--- tests/Connection/LazyTest.php
+++ tests/Connection/LazyTest.php
@@ -5,6 +5,7 @@ namespace Tests\Formal\AccessLayer\Connection;
 
 use Formal\AccessLayer\{
     Connection\Lazy,
+    Connection\PDO,
     Connection,
 };
 use Innmind\Url\Url;
@@ -76,6 +77,6 @@ class LazyTest extends TestCase
     {
         $port = \getenv('DB_PORT') ?: '3306';
 
-        return new Lazy(Url::of("mysql://root:root@127.0.0.1:$port/example"));
+        return new Lazy(static fn() => PDO::of(Url::of("mysql://root:root@127.0.0.1:$port/example")));
     }
 }
