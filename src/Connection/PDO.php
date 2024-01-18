c06a1280022b60c46fdc285da4c0f61e25a65bff
Merleur l'enchantin <le.neko@gmail.com>
2024-01-18 07:15:23 +0100

    handle unix_socket urls ex.: `mysql://root:root@/chemin/vers.socket/my_db

--- src/Connection/PDO.php
+++ src/Connection/PDO.php
@@ -29,7 +29,6 @@ final class PDO implements Connection
         $dsnPassword = $dsn->authority()->userInformation()->password();
         $user = null;
         $password = null;
-        $charset = '';
 
         if (!$dsnUser->equals(User::none())) {
             $user = $dsnUser->toString();
@@ -39,6 +38,17 @@ final class PDO implements Connection
             $password = $dsnPassword->toString();
         }
 
+        $this->pdo = new \PDO(
+            self::parseDsn($dsn),
+            $user,
+            $password,
+            $options,
+        );
+    }
+
+    private function parseDsn(Url $dsn): string
+    {
+        $charset = '';
         if (!$dsn->query()->equals(UrlQuery::none())) {
             \parse_str($dsn->query()->toString(), $query);
 
@@ -48,14 +58,29 @@ final class PDO implements Connection
             }
         }
 
-        $this->pdo = new \PDO(\sprintf(
+        // si pas de port alors socket
+        if (!$dsn->authority()->port()->value()) {
+            $path = $dsn->path()->toString();
+            $dbName = \basename($path);
+            $socketPath = \dirname($path);
+
+            return \sprintf(
+                '%s:unix_socket=%s;dbname=%s%s',
+                $dsn->scheme()->toString(),
+                $socketPath,
+                $dbName,
+                $charset,
+            );
+        }
+
+        return \sprintf(
             '%s:host=%s;port=%s;dbname=%s%s',
             $dsn->scheme()->toString(),
             $dsn->authority()->host()->toString(),
             $dsn->authority()->port()->toString(),
             \substr($dsn->path()->toString(), 1), // substring to remove leading '/'
             $charset,
-        ), $user, $password, $options);
+        );
     }
 
     public function __invoke(Query $query): Sequence
